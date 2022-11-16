ac73911a2868e1e56ef3cf5349a5690b263a1bae
vladbakumenko <klaid-slaid@yandex.ru>
2022-11-16 14:49:17 +0400

    Initial commit

---
+++ src/main/java/com/example/dictionarybot/DictionaryBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.dictionarybot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DictionaryBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DictionaryBotApplication.class, args);
+    }
+
+}
